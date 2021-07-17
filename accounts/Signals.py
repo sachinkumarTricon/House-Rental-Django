bc39c8a16a72877d905874a824a06c4c8d4e3ac7
Sachin Kumar <sachinkumar72353@gmail.com>
2021-07-17 13:21:28 +0530

    Update Signals.py

--- accounts/Signals.py
+++ accounts/Signals.py
@@ -20,7 +20,7 @@ def save_profile(sender,instance,created,**kwargs):
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
-    email_plaintext_message = "http://127.0.0.1:8000{}confirm/{}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+    email_plaintext_message = "http://127.0.0.1:8000/api/password-reset-confirmation/{}".format(reset_password_token.key)
 
     send_mail(
         # title:
