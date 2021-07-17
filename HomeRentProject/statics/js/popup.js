fba309d1a4dc16dfe990c9167abee6b7c5992fbd
Sachin Kumar <sachinkumar72353@gmail.com>
2021-07-17 13:06:24 +0530

    Update popup.js

--- HomeRentProject/statics/js/popup.js
+++ HomeRentProject/statics/js/popup.js
@@ -71,7 +71,9 @@ function verifyPhone(){
            $("#verifyOTPModal").modal()
         }
         else{
+             alert(data.Detail)
              window.location.href = "/rent/home"
+
         }
 
     })
@@ -207,7 +209,7 @@ var loginform = document.getElementById('loginform')
 
 loginform.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log('Clicked on VerifyOTP...')
+    console.log('Clicked on login...')
     login()
 //    document.getElementById('form-button').classList.add("hidden");
 
@@ -244,7 +246,104 @@ function login(){
         if (data.status){
             Auth_Token = data.token
             localStorage.setItem('Auth_Token',Auth_Token)
-            console.log("ye auth token hai login wala : ",Auth_Token)
+
+            window.location.href = "/rent/home"
+        }
+        else{
+          alert("Invalid Credentials")
+
+        }
+
+     }
+
+
+}
+
+// open login modal and close register modal
+
+function openLogin_Modal(){
+  document.getElementById('CloseRegModal').click()
+  $("#loginModal").modal()
+
+}
+
+
+
+// open Reset Password through Email and close login modal
+
+function openReset_Pass_Modal(){
+  document.getElementById('loginModal').click()
+  $("#reset_pass_modal").modal()
+
+}
+
+
+
+
+
+
+// Change Password API Call
+function OnPassChange(){
+  var changePwdForm = document.getElementById('changePwdForm')
+
+  changePwdForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('Clicked on ChngPassword...')
+
+    newpass = changePwdForm.new_password.value
+    cnfPass = changePwdForm.comfirm_password.value
+
+    console.log(newpass,cnfPass,"Ye new or confirm password ha")
+    if (newpass == cnfPass ){
+
+
+         ChngPass()
+
+     }
+     else{
+     alert("New Password not matched with confirm password Try again")
+     }
+//    document.getElementById('confirm-change').classList.add("hidden");
+
+  })
+
+}
+
+
+var ChngPassformData = {
+        'new_password':null,
+        'old_password': null
+    }
+
+
+
+function ChngPass(){
+
+    ChngPassformData.new_password = changePwdForm.new_password.value
+    ChngPassformData.old_password = changePwdForm.old_password.value
+    var auth_token = localStorage.getItem('Auth_Token')
+    var url = "http://127.0.0.1:8000/api/change-password"
+    fetch(url, {
+        method:'PUT',
+        headers:{
+            'Content-Type':'applicaiton/json',
+            'X-CSRFToken':csrftoken,
+            'Authorization': 'Token '+ auth_token
+
+
+        },
+        body:JSON.stringify(ChngPassformData),
+
+    })
+    .then((response) => response.json())
+    .then((data) =>checkstatus(data))
+
+
+
+     function checkstatus(data){
+        console.log(data.status)
+        if (data.status){
+            console.log(data)
             window.location.href = "/rent/home"
         }
         else{
@@ -255,5 +354,58 @@ function login(){
      }
 
 
+}
+
+
+//Reset password API call
+
+var resetpassform = document.getElementById('reset_pass_form')
+resetpassform.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('Clicked on Reset Password...')
+    resetPass()
+//    document.getElementById('form-button').classList.add("hidden");
+
+})
+
+
+function resetPass(){
+
+    var resetPassData = {
+
+        "email":null
+
+    }
+
+
+        resetPassData.email = resetpassform.reset_email.value
+        console.log(resetPassData.email)
+
+    var url = "http://127.0.0.1:8000/api/goToResetPass"
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type':'applicaiton/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({"email":resetPassData.email}),
+
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.status){
+           console.log(data)
+            window.location.href = "/rent/home"
+           alert("Password Reset Email has been sent to your registered email."+"     "+ "please check your email")
+
+        }
+        else{
+          alert("Failed to send email to reset password try again")
+           window.location.href = "/rent/home"
+        }
+
+
+
+    })
 }
 
